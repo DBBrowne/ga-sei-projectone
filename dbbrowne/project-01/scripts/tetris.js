0951355c4890512d1a1837e1549b458616e07aa3
dbbrowne <duncan.b.browne@gmail.com>
2021-11-11 19:24:58 +0000

    populate play matrix

--- dbbrowne/project-01/scripts/tetris.js
+++ dbbrowne/project-01/scripts/tetris.js
@@ -1,20 +1,42 @@
 //  * TETRIS
 
-function hello() {
+function testJestConnection() {
   console.log('hello')
   return 'hello'
 }
-hello()
+
 // * DOM Elements
+
+const playMatrixView = document.querySelector('.play-matrix')
 
 // * Variables
 
+const playMatrixHeight = 24
+const playMatrixWidth = 16
+const playMatrix = []
+
 // * Build play window
+function buildPlayMatrix(height, width){
+  for (let x = 0; x < height; x++){
+    playMatrix.push([])
+    for (let y = 0; y < width; y++){
+      const playCell = document.createElement('div')
+      playCell.textContent = `${x}, ${y}`
+      
+      playMatrixView.appendChild(playCell)
+      playMatrix[x].push(playCell)
+    }
+  }
+  return playMatrix
+}
+
+buildPlayMatrix(playMatrixHeight, playMatrixWidth)
 
 // * Functions
 
 // * Events
 
 module.exports = {
-  hello,
+  testJestConnection,
+  buildPlayMatrix,
 }
--- dbbrowne/project-01/scripts/tetris.test.js
+++ dbbrowne/project-01/scripts/tetris.test.js
@@ -2,5 +2,14 @@
 const tetr = require('./tetris.js')
 
 it('should work', ()=>{
-  expect(tetr.hello()).toBe('hello')
+  expect(tetr.testJestConnection()).toBe('hello')
 })
+
+
+// too much work to mock document at this point in my career.
+// describe('buildPlayMatrix',()=>{
+//   it('should build an array of length = playWidth * playHeight', ()=>{
+//     it('should return a length of 384 with = 24, h=16')
+//     expect(tetr.buildPlayMatrix(24,16).flat().length).toEqual(384)
+//   })
+// })
