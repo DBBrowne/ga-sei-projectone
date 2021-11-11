d747cffea02af6024445877a6255d1e8ce80540e
dbbrowne <duncan.b.browne@gmail.com>
2021-11-11 20:33:27 +0000

    attempt jest jsDOM

--- dbbrowne/project-01/scripts/tetris.test.js
+++ dbbrowne/project-01/scripts/tetris.test.js
@@ -1,10 +1,17 @@
 /* eslint-disable no-undef */
+/**
+ * @jest-environment jsdom
+ */
 const tetr = require('./tetris.js')
 
 it('should work', ()=>{
   expect(tetr.testJestConnection()).toBe('hello')
 })
 
+it('should use jsdom in this test file', () => {
+  const element = document.createElement('div')
+  expect(element).not.toBeNull()
+})
 
 // too much work to mock document at this point in my career.
 // describe('buildPlayMatrix',()=>{
