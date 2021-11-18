1c7eaffd703e80bb76ea78c0837d35a1063ecf1b
dbbrowne <duncan.b.browne@gmail.com>
2021-11-18 22:28:43 +0000

    local storage and highscores

--- scripts/tetris.js
+++ scripts/tetris.js
@@ -28,6 +28,7 @@ const htmlRoot = document.documentElement
 // todo: refactor to enum
 const isDebugMode = false
 const isDebugVerbose = false
+const localStorageDebugMode = true
 
 const redefineKeyMode = { 
   isOn: false, 
@@ -40,6 +41,7 @@ const playMatrixHeight = 20
 const playMatrixWidth = 16
 
 const tetrominoSpawnXY = [7,20]
+const localHiscoresStorageKey = 'tentris-hiscores'
 
 const defaultGameTickTime = 500
 const levelUpTickTimeMultiplier = 0.90
@@ -557,6 +559,90 @@ class Tetromino {
     this.update()
   }
 }
+// **************
+// * local storage manager
+const localHiscoresManager = {
+  records: [],
+  storageKey: localHiscoresStorageKey,
+  setStoredHiscores(newHiScores = this.records, key = this.storageKey){
+    const newHiscoresString = JSON.stringify(newHiScores.map((record=>JSON.stringify(record))))
+
+    localStorage.setItem(key, newHiscoresString)
+
+    localStorageDebugMode && console.log('new local highscores : ',newHiscoresString)
+
+    return newHiscoresString
+  },
+  populateLocalHighscores(key = this.storageKey){
+    // if key does not exist set to default.  If key exists already, set this.records and return parsed contents.
+
+    const localStorageItem = localStorage.getItem(key)
+
+    localStorageDebugMode && console.log('local storage on key', key,':' , localStorageItem)
+
+    if (!localStorageItem){
+      localStorageDebugMode && console.log('no Hiscores found.  Setting to default')
+
+      const defaultHiscores = [ { playerName: 'abc', score: 1000 }, { playerName: 'def', score: 900 },{ playerName: 'ghi', score: 800 },{ playerName: 'jkl', score: 700 },{ playerName: 'mno', score: 600 },{ playerName: 'pqr', score: 500 },{ playerName: 'stu', score: 400 },{ playerName: 'vwx', score: 300 },{ playerName: 'yza',score: 100 },{ playerName: 'bcd', score: 50 } ]
+
+      this.records = defaultHiscores
+      this.updateHiscoreScroller()
+
+      return this.setStoredHiscores(defaultHiscores)
+    }
+
+    const parsedLocalItem = JSON.parse(localStorageItem).map(record=>JSON.parse(record))
+      
+    this.records = parsedLocalItem
+
+    localStorageDebugMode && console.log('post-parsing records', this.records)
+
+    this.updateHiscoreScroller()
+    return parsedLocalItem
+  },
+  addNewRecordToHiscores(playerName, newScore){
+    this.records.pop()
+    this.records.push({ playerName: playerName, score: newScore })
+    this.records.sort((a, b) => b.score - a.score)
+    this.setStoredHiscores()
+    return true
+  },
+  checkNewHiscore(newScore){
+    const lowestScore = this.records[this.records.length - 1].score
+
+    if (newScore < lowestScore){
+      localStorageDebugMode && console.log('Score lower than minimum')
+      return false
+    }
+    localStorageDebugMode && console.log('New High Score!')
+    const newRecordPlayerName = this.capturePlayerName()
+    this.addNewRecordToHiscores(newRecordPlayerName, newScore)
+
+    return this.records
+  },
+  capturePlayerName(){
+    return window.prompt('NEW HIGH SCORE!\n\nEnter your name:') || 'PLAYER'
+  },
+  updateHiscoreScroller(newHiscores = this.records){
+    const hiscorePlayersElement = document.querySelector('.hiscore-players')
+    const hiscoreScoresElement = document.querySelector('.hiscore-scores')
+
+    //clear old list and initialise new list titles
+    hiscorePlayersElement.innerHTML = '<li>high</li>'
+    hiscoreScoresElement.innerHTML = '<li>scores</li>'
+
+    newHiscores.forEach(record=>{
+      const newPlayerElement = document.createElement('li')
+      const newScoreElement = document.createElement('li')
+      
+      newPlayerElement.textContent = record.playerName
+      newScoreElement.textContent =  record.score
+
+      hiscorePlayersElement.appendChild(newPlayerElement)
+      hiscoreScoresElement.appendChild(newScoreElement)
+    })
+  },
+}
 
 // ************
 // * global functions
@@ -786,8 +872,11 @@ if (isDebugMode){
 window.onresize =  handleWindowResize
 
 // **************************************************************************
+// START GAME
 // populate with at least one player
 globalPlayers.push(new TetrisGame)
+// init highscores
+localHiscoresManager.populateLocalHighscores()
 
 // **************************************************************************
 // * export functions for testing
