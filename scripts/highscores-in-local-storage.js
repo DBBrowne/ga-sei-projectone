dcc94fc99aa285bec81b0a1289ce40fa05ef76c6
dbbrowne <duncan.b.browne@gmail.com>
2021-11-18 16:17:55 +0000

    local hiscores with objects

--- scripts/highscores-in-local-storage.js
+++ scripts/highscores-in-local-storage.js
@@ -7,23 +7,38 @@ const winningscore = 650
 
 storageDebugMode && console.log('local storage', localHiscoresStorageKey,':' , localStorage.getItem(localHiscoresStorageKey))
 
-if (!localStorage.getItem(localHiscoresStorageKey)){
-  const defaultHiscores = JSON.stringify(
-    [1000,900,800,700,600,500,400,300,100,50]
-  )
-  localStorage.setItem(localHiscoresStorageKey, defaultHiscores)
+function setLocalHiscores(newHiScores, key=localHiscoresStorageKey){
+  const newHiscoresString = JSON.stringify(newHiScores.map((record=>JSON.stringify(record))))
+  localStorage.setItem(key, newHiscoresString)
+
+  storageDebugMode && console.log('new local highscores : ',newHiscoresString)
+  return newHiscoresString
 }
 
-const localHiscores = JSON.parse(localStorage.getItem(localHiscoresStorageKey))
+if (!localStorage.getItem(localHiscoresStorageKey)){
+  const defaultHiscores = [ { playerName: 'abc', score: 1000 }, { playerName: 'def', score: 900 },{ playerName: 'ghi', score: 800 },{ playerName: 'jkl', score: 700 },{ playerName: 'mno', score: 600 },{ playerName: 'pqr', score: 500 },{ playerName: 'stu', score: 400 },{ playerName: 'vwx', score: 300 },{ playerName: 'yza',score: 100 },{ playerName: 'bcd', score: 50 } ]
+  
+  setLocalHiscores(defaultHiscores)
+}
+
+const localHiscores = JSON.parse(
+  localStorage.getItem(localHiscoresStorageKey)
+)
 
 storageDebugMode && console.log(localHiscores)
 
-const lowestHiscore = localHiscores[localHiscores.length - 1]
+if (winningscore > localHiscores[localHiscores.length - 1].score){
+  storageDebugMode && console.log('New High Score!')
+  //capture name
+  const playerName = 'winrararar'
 
-if (winningscore > lowestHiscore){
+  const newHiscore = { playerName: playerName, score: winningscore }
+
   localHiscores.pop()
-  localHiscores.push(winningscore)
-  localHiscores.sort((a, b) => b - a)
-}
+  console.log('popped', localHiscores)
+  localHiscores.push(newHiscore)
+  localHiscores.sort((a, b) => b.score - a.score)
 
+  localStorage.setItem(localHiscoresStorageKey, localHiscores)
+}
 console.log(localHiscores)
